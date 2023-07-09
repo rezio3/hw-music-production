5e7e7df38d7391451be6ee2b2a35c1beb42bf3c1
Joszko <jakub.rezler96@gmail.com>
2023-07-09 17:56:34 +0200

    update Skills.jsx and styles

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "../style/css/skills.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faTelevision,
+	faTheaterMasks,
+	faGamepad,
+	faMusic,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Skills = () => {
-	return <section className="skills-section"></section>;
+	return (
+		<section className="skills-section">
+			<ul className="skills-container">
+				<li className="skills-container__skill">
+					<FontAwesomeIcon icon={faTelevision} className="icon" />
+					<span>Film & TV</span>
+				</li>
+				<li className="skills-container__skill">
+					<FontAwesomeIcon icon={faTheaterMasks} className="icon" />
+					<span>Theatre</span>
+				</li>
+				<li className="skills-container__skill">
+					<FontAwesomeIcon icon={faGamepad} className="icon" />
+					<span>Games</span>
+				</li>
+				<li className="skills-container__skill">
+					<FontAwesomeIcon icon={faMusic} className="icon" />
+					<span>Theater</span>
+				</li>
+			</ul>
+		</section>
+	);
 };
 
 export default Skills;
