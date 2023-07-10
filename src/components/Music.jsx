ec6590b5b6dab0d4b3f66b91fca40a75303b1700
Joszko <jakub.rezler96@gmail.com>
2023-07-10 11:24:24 +0200

    update Music.jsx component

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "../style/css/music.css";
 
 const Music = () => {
-	return <section className="music-section"></section>;
+	return (
+		<section className="music-section">
+			<div className="music-boxes-container">
+				<div className="music-boxes-container__music-box box-1"></div>
+				<div className="music-boxes-container__music-box box-2"></div>
+				<div className="music-boxes-container__music-box box-3"></div>
+				<div className="music-boxes-container__music-box box-4"></div>
+				<div className="music-boxes-container__music-box box-5"></div>
+				<div className="music-boxes-container__music-box box-6"></div>
+				<div className="music-boxes-container__music-box box-7"></div>
+				<div className="music-boxes-container__music-box box-8"></div>
+			</div>
+		</section>
+	);
 };
 
 export default Music;
