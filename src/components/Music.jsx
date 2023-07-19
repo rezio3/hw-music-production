ad5077320c9c203839dedb6772d77e80cc18b6d4
Joszko <jakub.rezler96@gmail.com>
2023-07-19 23:03:57 +0200

    delete animation in Music.jsx component

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,56 +1,55 @@
 import React, { useEffect, useRef } from "react";
 import "../style/css/music.css";
-// import { TweenMax } from "gsap/gsap-core";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Music = () => {
-	const scrollContainer1 = useRef(null);
-	const scrollContainer2 = useRef(null);
-	const scrollContainer3 = useRef(null);
-	const scrollContainer4 = useRef(null);
+	// const scrollContainer1 = useRef(null);
+	// const scrollContainer2 = useRef(null);
+	// const scrollContainer3 = useRef(null);
+	// const scrollContainerBG = useRef(null);
 	useEffect(() => {
-		let el = scrollContainer1.current;
-		gsap.to(el, {
-			xPercent: -100 * 1,
-			translateY: "0px",
-			ease: "none",
-			scrollTrigger: {
-				trigger: ".music-boxes-container1",
-				scrub: true,
-				// pin: true,
-				start: "top bottom",
-				end: "+=1500",
-			},
-		});
-		let el2 = scrollContainer2.current;
-		gsap.to(el2, {
-			xPercent: -100 * -1,
-			translateY: "0px",
-			ease: "none",
-			scrollTrigger: {
-				trigger: ".music-boxes-container2",
-				scrub: true,
-				// pin: true,
-				start: "top bottom",
-				end: "+=1500",
-			},
-		});
-		let el3 = scrollContainer3.current;
-		gsap.to(el3, {
-			xPercent: -100 * 1,
-			translateY: "0px",
-			ease: "none",
-			scrollTrigger: {
-				trigger: ".music-boxes-container3",
-				scrub: true,
-				// pin: true,
-				start: "top bottom",
-				end: "+=1500",
-			},
-		});
+		// let el = scrollContainer1.current;
+		// gsap.to(el, {
+		// 	xPercent: -100 * 1,
+		// 	// translateY: "0px",
+		// 	ease: "none",
+		// 	scrollTrigger: {
+		// 		trigger: ".music-boxes-container1",
+		// 		scrub: true,
+		// 		// pin: true,
+		// 		start: "top bottom",
+		// 		end: "+=1500",
+		// 	},
+		// });
+		// let el2 = scrollContainer2.current;
+		// gsap.to(el2, {
+		// 	xPercent: -100 * -1,
+		// 	// translateY: "0px",
+		// 	ease: "none",
+		// 	scrollTrigger: {
+		// 		trigger: ".music-boxes-container2",
+		// 		scrub: true,
+		// 		// pin: true,
+		// 		start: "top bottom",
+		// 		end: "+=1500",
+		// 	},
+		// });
+		// let el3 = scrollContainer3.current;
+		// gsap.to(el3, {
+		// 	xPercent: -100 * 1,
+		// 	// translateY: "0px",
+		// 	ease: "none",
+		// 	scrollTrigger: {
+		// 		trigger: ".music-boxes-container3",
+		// 		scrub: true,
+		// 		// pin: true,
+		// 		start: "top bottom",
+		// 		end: "+=1500",
+		// 	},
+		// });
 	}, []);
 	return (
 		<section className="music-section">
@@ -59,7 +58,9 @@ const Music = () => {
 				ref={scrollContainer1}
 			>
 				<div className="music-boxes-container__music-box box-1">
-					<h2 className="box-header">Into the Wild - Orchestral composition</h2>
+					<div className="music-boxes-container__shadow">
+						{/* <h2 className="box-header">Into the Wild - Orchestral composition</h2> */}
+					</div>
 				</div>
 				<div className="music-boxes-container__music-box box-2"></div>
 				<div className="music-boxes-container__music-box box-3"></div>
