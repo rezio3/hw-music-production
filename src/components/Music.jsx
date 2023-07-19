58d7b649824312c1777a5528d5d7962c015b5a6f
Joszko <jakub.rezler96@gmail.com>
2023-07-20 01:49:52 +0200

    modify Music.jsx component

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -5,13 +5,17 @@ const Music = () => {
 	useEffect(() => {}, []);
 	return (
 		<section className="music-section">
-			<div className="music-boxes-container music-boxes-container1">
+			<h3 className="music-section__header">Muzyka</h3>
+			<div className="music-boxes-container">
 				<div className="music-boxes-container__music-box box-1">
 					<div className="music-boxes-container__shadow">
 						<span className="box-text">
 							Into the Wild - Orchestral composition <br />
 							by Hubert Walkowski
 						</span>
+					</div>
+					<div className="music-boxes-container__box-footer">
+						<p>Eloo</p>
 					</div>
 				</div>
 				<div className="music-boxes-container__music-box box-2">
@@ -21,12 +25,18 @@ const Music = () => {
 							by Hubert Walkowski
 						</span>
 					</div>
+					<div className="music-boxes-container__box-footer">
+						<p>Eloo</p>
+					</div>
 				</div>
 				<div className="music-boxes-container__music-box box-3">
 					<div className="music-boxes-container__shadow">
 						<span className="box-text">
 							Muzyka do filmu "Pokot" <br /> Zwycięski utwór FMF Kraków 2019
 						</span>
+					</div>
+					<div className="music-boxes-container__box-footer">
+						<p>Eloo</p>
 					</div>
 				</div>
 				<div className="music-boxes-container__music-box box-4">
@@ -36,6 +46,9 @@ const Music = () => {
 							<br /> BIFSC - Berlin International Film Scoring Competition 2021
 						</span>
 					</div>
+					<div className="music-boxes-container__box-footer">
+						<p>Eloo</p>
+					</div>
 				</div>
 				<div className="music-boxes-container__music-box box-5">
 					<div className="music-boxes-container__shadow">
@@ -43,6 +56,9 @@ const Music = () => {
 							Muzyka do short-filmu "Escape"
 							<br /> BIFSC - Berlin International Film Scoring Competition 2020
 						</span>
+					</div>
+					<div className="music-boxes-container__box-footer">
+						<p>Eloo</p>
 					</div>
 				</div>
 				<div className="music-boxes-container__music-box box-6">
@@ -52,23 +68,32 @@ const Music = () => {
 							<br /> Spitfire Scoring Competition 2021
 						</span>
 					</div>
+					<div className="music-boxes-container__box-footer">
+						<p>Eloo</p>
+					</div>
 				</div>
 				<div className="music-boxes-container__music-box box-7">
 					<div className="music-boxes-container__shadow">
 						<span className="box-text">
-							Ścieżka dźwiękowa do gry ""
+							Ścieżka dźwiękowa do gry "coś tam"
 							<br />
-							by Hubert Walkowski
+							MeanTime Games
 						</span>
+					</div>
+					<div className="music-boxes-container__box-footer">
+						<p>Eloo</p>
 					</div>
 				</div>
 				<div className="music-boxes-container__music-box box-8">
 					<div className="music-boxes-container__shadow">
 						<span className="box-text">
-							Muzyka do spektaklu "coś tam Orfa"
+							Muzyka do spektaklu "coś tam Offa"
 							<br />
 							AST Wrocław
 						</span>
+					</div>
+					<div className="music-boxes-container__box-footer">
+						<p>Eloo</p>
 					</div>
 				</div>
 				<div className="music-boxes-container__music-box box-1">
@@ -77,6 +102,9 @@ const Music = () => {
 							Into the Wild - Orchestral composition <br />
 							by Hubert Walkowski
 						</span>
+					</div>
+					<div className="music-boxes-container__box-footer">
+						<p>Eloo</p>
 					</div>
 				</div>
 			</div>
