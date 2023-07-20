69e90cbf6d6061e54f571bc218ff9e6b196f57e0
Joszko <jakub.rezler96@gmail.com>
2023-07-20 02:49:17 +0200

    create separate array in music-boxes-data.js

---
+++ src/components/music-boxes-data.js
@@ -0,0 +1,56 @@
+export const musicBoxesData = [
+	{
+		className: "music-boxes-container__music-box box-1",
+		txt1: "	Into the Wild - Orchestral composition",
+		txt2: "by Hubert Walkowski",
+		id: 1,
+	},
+	{
+		className: "music-boxes-container__music-box box-2",
+		txt1: "Lucid Dreams - Orchestral Composition",
+		txt2: "by Hubert Walkowski",
+		id: 2,
+	},
+	{
+		className: "music-boxes-container__music-box box-3",
+		txt1: "Muzyka do filmu Pokot",
+		txt2: "Zwycięski utwór FMF Kraków 2019",
+		id: 3,
+	},
+	{
+		className: "music-boxes-container__music-box box-4",
+		txt1: "Muzyka do short-filmu Abiogenesis",
+		txt2: "BIFSC - Berlin International Film Scoring Competition 2021",
+		id: 4,
+	},
+	{
+		className: "music-boxes-container__music-box box-5",
+		txt1: "Muzyka do short-filmu Escape",
+		txt2: "BIFSC - Berlin International Film Scoring Competition 2020",
+		id: 5,
+	},
+	{
+		className: "music-boxes-container__music-box box-6",
+		txt1: "Muzyka do serialu Westworld",
+		txt2: "Spitfire Scoring Competition 2021",
+		id: 6,
+	},
+	{
+		className: "music-boxes-container__music-box box-7",
+		txt1: "Ścieżka dźwiękowa do gry",
+		txt2: "MeanTime Games",
+		id: 7,
+	},
+	{
+		className: "music-boxes-container__music-box box-8",
+		txt1: "Muzyka do spektaklu coś tam Offa",
+		txt2: "	AST Wrocław",
+		id: 8,
+	},
+	{
+		className: "music-boxes-container__music-box box-1",
+		txt1: "Into the Wild - Orchestral composition",
+		txt2: "by Hubert Walkowski",
+		id: 9,
+	},
+];
