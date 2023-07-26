bb28e15b83f028001570f34ce434cbb2bdb55055
Joszko <jakub.rezler96@gmail.com>
2023-07-26 15:55:32 +0200

    create animation in MusicBoxes.jsx component

--- src/components/MusicBoxes.jsx
+++ src/components/MusicBoxes.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const MusicBox = (props) => {
+	const [anim, setAnim] = useState(props.class);
+	const [animDone, setAnimDone] = useState(false);
+	const musicBoxes = useRef();
+
+	useEffect(() => {
+		const observer = new IntersectionObserver((entries) => {
+			const entry = entries[0];
+
+			if (entry.isIntersecting) {
+				setAnim(props.class + " music-box-anim-in");
+				setAnimDone(true);
+			} else if (!animDone) {
+				setAnim(props.class);
+			}
+		});
+		observer.observe(musicBoxes.current);
+	}, []);
+
+	console.log(anim);
 	return (
-		<div className={props.class}>
+		<div className={anim} ref={musicBoxes}>
 			<div className="music-boxes-container__shadow">
 				<span className="box-text">{props.txt1}</span>
 				<span className="box-text">{props.txt2}</span>
