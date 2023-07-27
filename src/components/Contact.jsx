5ad5709f11e04743db482885b43455077eb1fd86
Joszko <jakub.rezler96@gmail.com>
2023-07-27 17:42:08 +0200

    update Contact.jsx component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "../style/css/contact.css";
 
 const Contact = () => {
-	return <section className="contact-section"></section>;
+	return (
+		<section className="contact-section">
+			<div className="contact-container">
+				<h2 className="contact-container__header">Skontaktuj się</h2>
+				<button className="contact-container__btn">
+					Napisz{" "}
+					<span className="material-symbols-outlined arrow">navigate_next</span>
+				</button>
+			</div>
+		</section>
+	);
 };
 
 export default Contact;
