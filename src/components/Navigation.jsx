a7bdab60aeef03ff4b075ea9176d7de12ad1026e
Joszko <jakub.rezler96@gmail.com>
2023-07-27 17:30:27 +0200

    add scroll behaviors to navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,42 @@
 import React from "react";
 import "../style/css/navigation.css";
+import { useScrollTo } from "react-use-window-scroll";
 
 const Navigation = () => {
+	const scrollTo = useScrollTo();
+	const scrollDownAboutMe = () => {
+		scrollTo({
+			top: 990,
+			left: 0,
+			behavior: "smooth",
+		});
+	};
+	const scrollDownMusic = () => {
+		scrollTo({
+			top: 2200,
+			left: 0,
+			behavior: "smooth",
+		});
+	};
+	const scrollDownFilm = () => {
+		scrollTo({
+			top: 4200,
+			left: 0,
+			behavior: "smooth",
+		});
+	};
 	return (
 		<nav className="nav-container">
 			<ul className="nav-buttons">
-				<li className="nav-buttons__button">O mnie</li>
-				<li className="nav-buttons__button">Muzyka</li>
-				<li className="nav-buttons__button">Film & TV</li>
+				<li className="nav-buttons__button" onClick={scrollDownAboutMe}>
+					O mnie
+				</li>
+				<li className="nav-buttons__button" onClick={scrollDownMusic}>
+					Muzyka
+				</li>
+				<li className="nav-buttons__button" onClick={scrollDownFilm}>
+					Film & TV
+				</li>
 				<li className="nav-buttons__button">Kontakt</li>
 			</ul>
 		</nav>
