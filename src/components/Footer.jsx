4aa502b66fb953663872746a6d72b485ceb2ad37
Joszko <jakub.rezler96@gmail.com>
2023-08-01 19:10:11 +0200

    update footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "../style/css/footer.css";
 
 const Footer = () => {
-	return <div className="footer-container"></div>;
+	return (
+		<div className="footer-container">
+			<span>Socials</span>
+		</div>
+	);
 };
 
 export default Footer;
