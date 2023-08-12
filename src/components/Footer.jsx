455462f1092ee29431f47ad08567dc474b2affcb
Joszko <jakub.rezler96@gmail.com>
2023-08-13 01:47:10 +0200

    add footer content to Footer.jsx component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {} from "@fortawesome/free-solid-svg-icons";
 import "../style/css/footer.css";
 
 const Footer = () => {
 	return (
 		<div className="footer-container">
-			<span>Socials</span>
+			<div className="footer-container__copyright-container">
+				<span>Hubert Walkowski 2023 &copy; All rights reserved</span>
+			</div>
+
+			<div className="footer-container__socials">
+				<i className="fa-brands fa-instagram"></i>
+				<i className="fa-brands fa-facebook"></i>
+				<i className="fa-brands fa-youtube"></i>
+				<i className="fa-brands fa-tiktok"></i>
+				<i className="fa-brands fa-soundcloud"></i>
+			</div>
 		</div>
 	);
 };
